223fd45c0ef580f47d9d1500722697d0787c6216
likhith <123664920+likhithlikki@users.noreply.github.com>
2023-09-09 00:31:59 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,25 +1,26 @@
-function clr(){
-    document.getElementById('tv').value=" ";
+function clr() {
+    document.getElementById('tv').value = "";
 }
 
-function disp(val){
-    document.getElementById('tv').value +=val
+function disp(val) {
+    document.getElementById('tv').value += val;
 }
 
-function clr1(){
-    let b=document.getElementById('tv').value;
-    c=b.slice(0,-1);
-    document.getElementById('tv').value=c;
+function clr1() {
+    let b = document.getElementById('tv').value;
+    let c = b.slice(0, -1);
+    document.getElementById('tv').value = c;
 }
 
 function ans() {
-    try{
-      let x=document.getElementById('tv').value;
-      let y= eval(x)
-      clr()
-      document.getElementById('tv').value=y
-    }
-    catch{
-        document.getElementById('tv').value="Error"
+    try {
+        let x = document.getElementById('tv').value;
+        let y = eval(x);
+        document.getElementById('tv').value = y;
+    } catch (error) {
+        document.getElementById('tv').value = "Error";
+        setTimeout(function () {
+            document.getElementById('tv').value = "";
+        }, 1500);
     }
 }
